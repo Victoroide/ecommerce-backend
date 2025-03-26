6ed5cd6d3b56cb449c21be13a14eda1a5eabe0f9
Victoroide <cvictorhugo39@gmail.com>
2025-03-26 11:27:44 -0400

    Refactor TimestampedModel to be abstract for better inheritance in models

--- app/models/timestamped.py
+++ app/models/timestamped.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, DateTime
 from app.models.base_class import Base
 from datetime import datetime
 
-class TimestampedModel(Base):
+class TimestampedModel:
+    __abstract__ = True
+    
     created_at = Column(DateTime, default=datetime.now(), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
